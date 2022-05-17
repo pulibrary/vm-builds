efa7bb1d65d13699a123c5d64cefc27569f2a8c3
Francis Kayiwa <kayiwa@pobox.com>
2022-05-17 09:42:41 -0400

    add an ansible playbook
    the playbook will add the keys for all the devs
    in addition add cleanup files
    
    Co-authored-by: Carolyn Cole <cac9@princeton.edu>

---
+++ scripts/install_tools.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eux
+
+## install base tools
+echo "==> Installing base tools"
+apt-get update -y
+apt-get install vim -y
+apt-get update -y
+apt-get install curl apt-transport-https ca-certificates wget -y
+apt-get update -y
+apt-get install build-essential -y
+apt-get update -y
+apt install python -y
+apt-get upgrade -y
