374ecdefb10e717cd09dc0ae778f689b9fc4d5ce
Francis Kayiwa <kayiwa@pobox.com>
2025-09-18 15:40:12 -0400

    remove all previous vmbuilds

--- templates/scripts/regenerate_ssh_keys.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "Regenerating SSH keys..."
-rm -f /etc/ssh/ssh_host_*
-dpkg-reconfigure openssh-server
-echo "SSH keys regenerated."
-systemctl disable regenerate-ssh-keys.service
--- templates/scripts/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash -eux
-
-# Add pulsys user to sudoers.
-echo "pulsys        ALL=(ALL)       NOPASSWD: ALL" >>/etc/sudoers
-sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
-
-# Disable daily apt unattended updates.
-echo 'APT::Periodic::Enable "0";' >>/etc/apt/apt.conf.d/10periodic
